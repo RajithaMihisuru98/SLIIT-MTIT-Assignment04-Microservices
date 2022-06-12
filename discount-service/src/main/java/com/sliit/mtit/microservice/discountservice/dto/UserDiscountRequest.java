341c7f215761b3b96c3adbdea5e239089b795960
VihangiWijesinghe <vihangi.wijesinghe@gmail.com>
2022-06-12 19:58:48 +0530

    User details and discounts functions are added

---
+++ discount-service/src/main/java/com/sliit/mtit/microservice/discountservice/dto/UserDiscountRequest.java
@@ -0,0 +1,22 @@
+package com.sliit.mtit.microservice.discountservice.dto;
+
+public class UserDiscountRequest {
+    private String userEmail;
+    private String userPass;
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPass() {
+        return userPass;
+    }
+
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
+    }
+}
