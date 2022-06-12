86623e7a192ae2f699a374388d7b3aa52693a573
hasantha32 <hasanthamadushan32@gmail.com>
2022-06-12 18:15:48 +0530

    User response classes finalized

---
+++ shipment-service/src/main/java/com/sliit/mtit/microservice/shipmentservice/dto/UserDetailResponse.java
@@ -0,0 +1,76 @@
+package com.sliit.mtit.microservice.shipmentservice.dto;
+
+public class UserDetailResponse {
+    private String successMessage;
+    private String successCode;
+    private String userName;
+    private String userShippingAddress;
+    private String userEmail;
+    private String userContact;
+    private String userPass;
+    private String userType;
+
+    public String getSuccessMessage() {
+        return successMessage;
+    }
+
+    public void setSuccessMessage(String successMessage) {
+        this.successMessage = successMessage;
+    }
+
+    public String getSuccessCode() {
+        return successCode;
+    }
+
+    public void setSuccessCode(String successCode) {
+        this.successCode = successCode;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserShippingAddress() {
+        return userShippingAddress;
+    }
+
+    public void setUserShippingAddress(String userShippingAddress) {
+        this.userShippingAddress = userShippingAddress;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserContact() {
+        return userContact;
+    }
+
+    public void setUserContact(String userContact) {
+        this.userContact = userContact;
+    }
+
+    public String getUserPass() {
+        return userPass;
+    }
+
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+}
