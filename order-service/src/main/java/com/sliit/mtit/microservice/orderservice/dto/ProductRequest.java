b49f51affdc5ebc1dccd0f9b81334ce96f1673de
DamikaSulaksen <damikadesilva@gmail.com>
2022-06-13 14:01:03 +0530

    Product request & response classes are added

---
+++ order-service/src/main/java/com/sliit/mtit/microservice/orderservice/dto/ProductRequest.java
@@ -0,0 +1,13 @@
+package com.sliit.mtit.microservice.orderservice.dto;
+
+public class ProductRequest {
+    private String productId;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+}
